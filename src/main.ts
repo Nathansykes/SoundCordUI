e8013f61686dc4bf859cbf5fd49f662d608ad272
Nathan Sykes <Nathan.Sykes1@hotmail.com>
2024-05-08 19:15:09 +0100

    comments

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,13 @@ import router from './router'
 import apiService from './api/services/api-service';
 
 
-
+// import boostrap
 import './assets/bootstrap/js/bootstrap.bundle.min.js';
-
 import './assets/bootstrap/css/bootstrap.min.css';
 import './assets/bootstrap/bootstrap-litera.css';
 import './assets/bootstrap/icons/bootstrap-icons.min.css';
+
+// import local css
 import './assets/css/main.css';
 
 //import css for plyr
@@ -18,6 +19,5 @@ import 'plyr/dist/plyr.css';
 apiService.post('wakeup');
 
 const app = createApp(App)
-//app.use(plugin)
 app.use(router)
 app.mount('#app')
